7d8bd6f2e247c9991878ed97cdeadbe0714b951a
Peter Senna Tschudin <peter.senna@gmail.com>
2013-08-17 15:27:12 +0200

    added warning note

--- Custom_Resolution/super-monitor.sh
+++ Custom_Resolution/super-monitor.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Peter Senna Tschudin - peter.senna@gmail.com
 #
+# Please note that there is no warranty that this script works for you, and
+# there is no warranty that this script is safe for your hardware. I'm quite
+# sure that playing with Modelines can burn some old CRT monitors. I've never
+# heard that you can burn LCD / LED monitors, but I'm not sure.
 
 # Monitor information
 H=2560
